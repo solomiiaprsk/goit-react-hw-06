8c1bd0f7b40384f03868045560f563ac0b6de814
solomiiaprsk <solomiia.pruska@gmail.com>
2024-11-09 10:35:24 -0700

    final

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import css from "./App.module.css";
 import ContactList from "../ContactList/ContactList";
 import ContactForm from "../ContactForm/ContactForm";
